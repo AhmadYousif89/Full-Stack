e7147eefe88a4cddc8ddcfbb5516b0f42c6e7ad0
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-03-26 22:17:04 +0200

    deploy

--- udagram-api/bin/deploy-eb.sh
+++ udagram-api/bin/deploy-eb.sh
@@ -1,4 +1,4 @@
 aws s3api create-bucket --bucket udagram-bknd
 aws s3 cp www/deploy-bknd.zip s3://udagram-bknd/deploy-bknd.zip
-aws elasticbeanstalk create-application-version --application-name udagram --version-label 12345 --source-bundle S3Bucket="udagram-bknd",S3Key="deploy-bknd.zip"
-aws elasticbeanstalk update-environment --application-name udagram --environment-name udagram-env-1 --version-label 12345
+aws elasticbeanstalk create-application-version --application-name udagram --version-label app-1 --source-bundle S3Bucket="udagram-bknd",S3Key="deploy-bknd.zip"
+aws elasticbeanstalk update-environment --application-name udagram --environment-name udagram-env --version-label app-1
