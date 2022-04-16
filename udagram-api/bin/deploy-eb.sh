2cd6d3b522fad99f632c8b2c015226533b765e1a
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-04-16 03:00:14 +0200

    up

--- udagram-api/bin/deploy-eb.sh
+++ udagram-api/bin/deploy-eb.sh
@@ -2,4 +2,7 @@
 # aws s3 cp www/deploy-bknd.zip s3://udagram-bknd/deploy-bknd.zip
 # aws elasticbeanstalk create-application-version --application-name udagram --version-label app-1 --source-bundle S3Bucket="udagram-bknd",S3Key="deploy-bknd.zip"
 # aws elasticbeanstalk update-environment --application-name udagram --environment-name Udagram-env --version-label app-1
-cd www && eb deploy Udagram-env
+eb init udagram -r $AWS_DEFAULT_REGION -p node.js
+eb use Udagram-env
+cd udagram-api/www/
+eb deploy Udagram-env
